2b012ce3107e65066d040c16cf2130b39be3ee36
Cyberchebs <chebemnzomblack@gmail.com>
2024-07-03 13:12:47 +0100

    little tweaks

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 //current utc code
-function updateUTCTime() {
-  const now = new Date();
-  const hours = now.getUTCHours().toString().padStart(2, "0");
-  const minutes = now.getUTCMinutes().toString().padStart(2, "0");
-  const seconds = now.getUTCSeconds().toString().padStart(2, "0");
-  const milliseconds = now.getUTCMilliseconds().toString().padStart(3, "0");
-  const timeString = `${hours}:${minutes}:${seconds}`;
-  document.getElementById("utc").textContent = timeString;
-}
+// function updateUTCTime() {
+//   const now = new Date();
+//   const hours = now.getUTCHours().toString().padStart(2, "0");
+//   const minutes = now.getUTCMinutes().toString().padStart(2, "0");
+//   const seconds = now.getUTCSeconds().toString().padStart(2, "0");
+//   const milliseconds = now.getUTCMilliseconds().toString().padStart(3, "0");
+//   const timeString = `${hours}:${minutes}:${seconds}`;
+//   document.getElementById("utc").textContent = timeString;
+// }
 
-updateUTCTime();
+// updateUTCTime();
 
-setInterval(updateUTCTime, 1);
+// setInterval(updateUTCTime, 1);
 
 //current day code
+
+function displayUTCTime() {
+  const currentTime = new Date();
+  let utcHours = currentTime.getUTCHours();
+  const utcMinutes = currentTime.getUTCMinutes().toString().padStart(2, "0");
+  const utcSeconds = currentTime.getUTCSeconds().toString().padStart(2, "0");
+  const utcMilliseconds = currentTime
+    .getUTCMilliseconds()
+    .toString()
+    .padStart(3, "0");
+
+  const ampm = utcHours >= 12 ? "PM" : "AM";
+  utcHours = utcHours % 12;
+  utcHours = utcHours ? utcHours : 12; // the hour '0' should be '12'
+  const utcHoursStr = utcHours.toString().padStart(2, "0");
+
+  const utcString = `${utcHoursStr}:${utcMinutes}:${utcSeconds}.${ampm}`;
+  document.getElementById("utc").innerText = utcString;
+}
+
+// Update the time every millisecond
+setInterval(displayUTCTime, 1);
+
+// Initial display
+displayUTCTime();
+
+//code for day
 
 function updateDayOfWeek() {
   const daysOfWeek = [
